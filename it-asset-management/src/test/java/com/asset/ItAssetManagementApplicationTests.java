164dc8ee476401409b799d7274261b8e53193b4e
suhas shivalkar <pavanshivalkar@gmail.com>
2025-02-27 15:37:50 +0530

    project directory structure

---
+++ it-asset-management/src/test/java/com/asset/ItAssetManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItAssetManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
